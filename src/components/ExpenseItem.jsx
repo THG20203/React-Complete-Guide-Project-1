4bbe547e02d9f6fdbedfc4fbba290c1bd01bea02
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-10 14:59:12 +0100

    more complex html structures inside of components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,11 @@
 function ExpenseItem() {
   return (
     <div>
-      <h2>Expense Item</h2>
+      <h2>March 28th 2021</h2>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
     </div>
   );
 }
