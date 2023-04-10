189d12c0f8e0e43693d0cfffe0f0f8d8e2e181c7
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-10 17:18:38 +0100

    splitting components

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,5 +1,7 @@
-/* Splitting components -> props need to be accepted here, such that a date prop is set */
+/* import css file for ExpenseItem.css -> make entire project aware of that css file */
+import "./ExpenseDate.css";
 
+/* Splitting components -> props need to be accepted here, such that a date prop is set */
 function ExpenseDate(props) {
   /* Output value dynamically and acces props.date. How can we output the month? We can use a built
   in method - accessible on all date objects in JavaScript - toLocaleString. This toLocaleString 
@@ -21,10 +23,10 @@ function ExpenseDate(props) {
   the year as a four digit number */
   const year = props.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
