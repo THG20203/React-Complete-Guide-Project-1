0e667dfc29f492e7415c12ee18d67b8d9d1d8667
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-10 23:51:45 +0100

    Splitting out Expense Form and the NewExpense component for cleaner code

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./NewExpense.css";
 
 /* NewExpense looking to render a form where users can enter/input their expense data */
 const NewExpense = () => {
   return (
-    <div>
+    <div className="new-expense">
       <form></form>
     </div>
   );
