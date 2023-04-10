9e276a1d1fa3e8e1139fa9c246480f1bff34f400
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-10 23:46:08 +0100

    beginning to look at adding form inputs

---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/* NewExpense looking to render a form where users can enter/input their expense data */
+const NewExpense = () => {
+  return (
+    <div>
+      <form></form>
+    </div>
+  );
+};
+
+export default NewExpense;
