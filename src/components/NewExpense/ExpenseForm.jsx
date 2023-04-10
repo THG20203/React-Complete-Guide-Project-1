bfcc6d93a4d1d35d90d873383b1298a8773ce0b7
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-10 23:57:21 +0100

    form markup added

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  return <form></form>;
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
