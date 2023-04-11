69cb63af5164af6d28acd5f43e8cd8334dcd5720
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-11 21:43:17 +0100

    beginning of adding process to listen to user input, utilising the onChange prop

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  /* best to add logic here and not within the JSX code like we have discussed before. To 
+  make it clear this will be called on an event - end the const name with handler */
+
+  /* function should be executed whenever the title input changes */
+  const titleChangeHandler = () => {};
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          {/* using onChange -> change on every keystroke rather than onClick -> advantage - can use same event 
+          on all input types */}
+
+          {/* we're changing the prop to onChange -> under the hood this will be an event listener for a change 
+          event to the input element which is rendered in the DOM. Then we need to point (at the function) that 
+          should be executed when that event occurs. */}
+
+          {/* in the onChange prop below I'll pass a pointer 'titleChangeHandler' as a value */}
+          <input type="text" onChange={titleChangeHandler} />
         </div>
       </div>
       <div className="new-expense__controls">
