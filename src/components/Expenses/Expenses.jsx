aed1a53043aaf7eb54203c3a727ddf6a0994aad8
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-17 18:49:14 +0100

    rendering lists of data

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -68,59 +68,20 @@ const Expenses = (props) => {
         and that function is then executed for every item in the array on which we're calling map. */}
         {/* The result of this function is tyhe element that will be added to the newly created array. */}
 
-        {props.items.map((expense) => (
-          {/* function body -> return the JSX element in which we want to map this expense. In this case
-        I want to map every expense in my expenses array (shown in parentheses above) into my JSX element */}
-        {/* In other words -> I want to transform the expense object to this JSX element. Want to transform
-        the expense object to this special kind of object. to this JSX element. */}
-        {/* This expense -> (which is passed as parameter into this function automatically), that expense 
+        {/* function body -> return the JSX element in which we want to map this expense. In this case
+        I want to map every expense in my expenses array (shown in parentheses above) into my JSX element 
+         In other words -> I want to transform the expense object to this JSX element. Want to transform
+        the expense object to this special kind of object. to this JSX element.
+        This expense -> (which is passed as parameter into this function automatically), that expense 
         is used to extract the title */}
-          <ExpenseItem title={expense.title} />
+
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
         ))}
-
-        {/* Uppercase character - custom components must start with uppercase character */}
-
-        {/* Passing data of 4 expense objects above into the expense items, so need to make expense items
-      configurable from outside. Data should not be stored inside, but received from outside */}
-
-        {/* In App.js - can simply add attrbitues to these custom HTML elements (components) */}
-
-        {/* Dynamically retrieve the title stored in this first ExpesneItem, (can use curly braces to assign
-        values to attributes) */}
-
-        {/* Access expenses array, within here first item with index 0, then in there the object -> with a 
-        title property. */}
-
-        {/* note the attribute names are up to me. Just note the part after the dot matches the property names,
-        because we are accessing these objects */}
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
-        {/* ExpenseItem is being used four times. In Expenses js, we have four ExpenseItems.
-      Now every item recieves its own seperate State, which is detached from the other states */}
-
-        {/* Everytime it is called, a new seperate state is created. (of course, this is in the 
-        same way, but its managed independently by React) */}
-
-        {/* so if we change the title with a button onClick in the first ExpenseItem, the other ones 
-      are not effected cause they have their own state. Its on a per component instance basis */}
       </Card>
     </div>
   );
