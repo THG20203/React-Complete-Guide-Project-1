520b8368f780215a842d29014df61fb01e7dcd92
THG20203 <tristanhgriffiths8@yahoo.com>
2023-04-19 21:49:59 +0100

    adding a dynamically outputting chart with data points prop and map function

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
 /* Create chart component function */
 const Chart = (props) => {
   /* inside chart -> render chart bars */
+  return (
+    <div className="chart">
+      {/* OVERALL PREMISE - output chart bars dynamically by going through array of data points and 
+      mapping every data point to a chart bar */}
+      {/* On props, could expect a data points prop, (that name is up to me, it is my component) and 
+      I expect data points prop to hold a value which is an array, hence calling map() -> then map()
+      every single data point into a Chart bar component. */}
+      {/* Aim of function below -> create as many chart bar components as we have dataPoints */}
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
